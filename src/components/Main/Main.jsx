e5025ffb6880f5300b30a40959e352c71e412849
Bugor_Petr <102902699+petyabugor@users.noreply.github.com>
2022-11-20 20:30:35 +0200

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import  './Main.css'
+
+function Main() {
+  return (
+    <div class="integrate">
+            <div class="what-can-you">
+            <div class="what-can-you__container _container-narrow">
+                <h3 class="what-can-you__title">Трендові товари</h3>
+                <div class="what-can-you__content">    
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">  
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                    <div class="what-can-you__item">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default Main
